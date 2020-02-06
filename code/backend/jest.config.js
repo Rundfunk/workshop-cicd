239c9dd0c985b4747126f1a502394de0a56a56d3
Ferdy van Varik <f.van.varik@blueriq.com>
2020-02-06 15:41:13 +0100

    Revert trying to disable color

--- code/backend/jest.config.js
+++ code/backend/jest.config.js
@@ -2,5 +2,4 @@ module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
   testRegex: process.env.NODE_ENV === 'production' ? '(/__tests__/.*|(\\.|/)(it-test|spec))\\.ts?$' : '(/__tests__/.*|(\\.|/)(test|spec))\\.ts?$',
-  colors: false,
 };
